ed89617da716f8ac88763a7e9d02f332d15b87d9
francescolucco <francescolucco@googlemail.com>
2022-01-25 17:51:12 +0100

    first commit

---
+++ database/migrations/2022_01_25_154224_create_holidays_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHolidaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('holidays', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title', 150);
+            $table->text('description')->nullable();
+            $table->string('address', 100);
+            $table->string('city', 50);
+            $table->string('state', 50);
+            $table->tinyInteger('overnight_stays')->unsigned();
+            $table->date('departure_date');
+            $table->decimal('price', 7, 2);
+            $table->boolean('flight_included')->unsigned()->default(1);
+            $table->boolean('insurance')->unsigned()->default(1);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('holidays');
+    }
+}
